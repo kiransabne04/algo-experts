902014e3020e65d9bafcf9332d389174cebd170e
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-05 06:02:23 +0530

    solving group anagrams

--- group-anagrams/main.go
+++ group-anagrams/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main(){
@@ -13,7 +14,21 @@ func main(){
 
 func GroupAnagrams(words []string) [][]string {
 	// Write your code here.
-	
-
+	wordsMap := make(map[string][]string)
+	for i, v := range words {
+		fmt.Println(i, v)
+		strRune := []rune(v)
+		sort.Slice(strRune, func(i, j int) bool {
+			return strRune[i] < strRune[j]
+		})
+		fmt.Println(string(strRune))
+		if w, ok := wordsMap[string(strRune)]; ok {
+			wordsMap[string(strRune)] = append(w, v)
+		} else {
+			wordsMap[string(strRune)] = append(w, v)
+		}
+		
+	}
+	fmt.Println(wordsMap)
 	return nil
 }
