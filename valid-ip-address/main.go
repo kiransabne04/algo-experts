88d556eeef44b285d087aed9d914d019bc0e0c84
Kiran Sabne <kiransabne04@gmail.com>
2024-09-20 11:33:03 +0530

    ip-address-valid-list

--- valid-ip-address/main.go
+++ valid-ip-address/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -12,9 +11,21 @@ func main() {
 
 func ValidIPAddresses(str string) []string {
 	// Write your code here.
-	ipNum, err := strconv.Atoi(str)
-	if err != nil {
-		
+	// lets create a list of IP sequences and then we will check for its validity
+	start := 1
+	//part := make([]string, 0)
+	for i := 0; i < 3; i++ {
+		// if the start+i position is > len of inputStr
+		if start+i > len(str) {
+			break
+		}
+
+		fmt.Println(string(str[i]), start)
+
+		//part = append(part, string(str[i]))
+		part := str[start : start+i]
+		//start = start + i
+		fmt.Println("startPntr -> ", string(part))
 	}
 	return []string{}
 }
