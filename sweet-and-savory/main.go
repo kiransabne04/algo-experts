85305eda94bef2e020380fb1cc24a0a350981889
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-10-11 06:29:27 +0530

    sweet & savory problem

--- sweet-and-savory/main.go
+++ sweet-and-savory/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 func main(){
-	dishes := []int{-3, -5, 1, 7}
-	target := 8
+	dishes := []int{2, 5, -4, -7, 12, 100, -25}
+	target := -7
 	val := SweetAndSavory(dishes, target)
 	fmt.Println(val)
 }
@@ -12,6 +16,32 @@ func main(){
 func SweetAndSavory(dishes []int, target int) []int {
 	// Write your code here.
 	res := []int{0,0}
-	
+	sort.Ints(dishes)
+
+	fmt.Println("dishes -> ", dishes)
+	start := 0
+	end := len(dishes) - 1
+	bestDiff := math.MaxInt64
+
+	for start < end && dishes[start] < 0 && dishes[end] > 0{
+		fmt.Println("start & end -> ", start, end)
+		currentSum := dishes[start] + dishes[end]
+		fmt.Println("currentSum -> ", currentSum)
+
+		if currentSum <= target {
+			currentDiff := target - currentSum
+			fmt.Println("currentDiff -> ", currentDiff)
+			if currentDiff < bestDiff {
+				bestDiff = currentDiff
+				res[0] = dishes[start]
+				res[1] = dishes[end]
+			}
+			fmt.Println("currentDiff , bestDiff-> ", currentDiff, bestDiff, res)
+			start++
+		} else {
+			end --
+		}
+	}
+
 	return res
 }
