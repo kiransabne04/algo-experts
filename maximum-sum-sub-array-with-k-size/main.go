9b37a8cfd8c9a72c28b1fbd5d53d34e7bc01cfc1
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-10-16 06:35:16 +0530

    maximum sum sub array with k size

---
+++ maximum-sum-sub-array-with-k-size/main.go
@@ -0,0 +1,15 @@
+package main
+
+//Given an array of integers and a number K, find the maximum sum of a subarray of size
+
+// Input: arr = [2, 1, 5, 1, 3, 2], K = 3
+// Output: 9
+// Explanation: Subarray with maximum sum is [5, 1, 3]
+
+func main() {
+
+}
+
+func MaxSumSubArraySizeK(arr []int, k int) int {
+	
+}
