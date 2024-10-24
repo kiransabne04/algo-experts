1b9b00b6ceaf9f5355e9133e6bf7fb6731d45788
Kiran Sabne <kiransabne04@gmail.com>
2024-10-24 17:15:40 +0530

    middle node

--- middle-node/main.go
+++ middle-node/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // This is an input struct. Do not edit.
 type LinkedList struct {
@@ -10,12 +12,11 @@ type LinkedList struct {
 
 func main() {
 
-	list := &LinkedList{Value: 1}
-	list.Next = &LinkedList{Value: 1}
-	list.Next.Next = &LinkedList{Value: 2}
-	list.Next.Next.Next = &LinkedList{Value: 3}
-	list.Next.Next.Next.Next = &LinkedList{Value: 3}
-
+	list := &LinkedList{Value: 2}
+	list.Next = &LinkedList{Value: 7}
+	list.Next.Next = &LinkedList{Value: 3}
+	list.Next.Next.Next = &LinkedList{Value: 5}
+	fmt.Println(list)
 	val := MiddleNode(list)
 	fmt.Println(val)
 }
@@ -25,8 +26,27 @@ func MiddleNode(linkedList *LinkedList) *LinkedList {
 	count := 1
 	current := linkedList
 
-	for linkedList.Next != nil {
+	for current != nil {
+		fmt.Println("current & current.Next -> ", current.Value, current.Next)
+		count++
+		current = current.Next
+	}
+	fmt.Println("count -> ", count/2)
+	if count%2 == 0 {
+		count = count / 2
+	} else {
+		count = (count / 2) + 1
+	}
+	fmt.Println("counrt -> ", count)
+	//fmt.Println("current & current.Next -> ", current)
 
+	for linkedList != nil {
+		count--
+		fmt.Println("linkedList & linkedList.Next -> ", linkedList.Value, linkedList.Next, count)
+		if count == 0 {
+			return linkedList
+		}
+		linkedList = linkedList.Next
 	}
 
 	return nil
